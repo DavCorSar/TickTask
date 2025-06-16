ec87417956c3ecb1f855581200d2cef1f246194c
David <dcs1460@gmail.com>
2025-06-17 00:16:19 +0200

    feat: Definition of the home page

---
+++ gui/pages/home.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-container fluid class="fill-height">
+        <Home />
+    </v-container>
+</template>
+
+<script setup>
+
+definePageMeta({
+    middleware: 'auth',
+    requiresAuth: true,
+    layout: 'newdefault'
+})
+</script>
