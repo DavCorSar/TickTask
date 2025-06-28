974263de303db7e1adc2adad6b8a61753f147868
David <dcs1460@gmail.com>
2025-06-29 01:05:29 +0200

    feat: Creation of a new layout that contains the left menu once the user is logged

---
+++ gui/layouts/defaultlogged.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <LeftMenu />
+    <v-main class="my-10 mx-10">
+      <NuxtPage />
+      <!-- Aquí se cargarán las páginas dinámicamente -->
+    </v-main>
+    <!-- <Footer /> -->
+  </v-app>
+</template>
+
+<script setup></script>
