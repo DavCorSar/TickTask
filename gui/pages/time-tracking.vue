715c744d7dbd5dbc7d8ee89c2223d33b4956b932
David <dcs1460@gmail.com>
2025-06-29 01:07:50 +0200

    feat: Creation of the page that will allow the user to visit and review all checked activities

---
+++ gui/pages/time-tracking.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container fluid>
+    <span class="mt-2 text-lg font-semibold text-gray-800"
+      >TODO: Define the page that will contain the time-tracker</span
+    >
+  </v-container>
+</template>
+
+<script setup>
+  definePageMeta({
+    middleware: "auth",
+    requiresAuth: true,
+    layout: "defaultlogged",
+  });
+</script>
