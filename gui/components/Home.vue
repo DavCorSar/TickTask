1ceccdde14b19f4cf5e0e27b3cabdf275b016e43
David <dcs1460@gmail.com>
2025-06-29 01:02:39 +0200

    feat: Include new buttons that redirect to the pages to check-in / out and to visit the history

--- gui/components/Home.vue
+++ gui/components/Home.vue
@@ -1,38 +1,41 @@
 <template>
+  <v-container class="mx-auto">
+    <v-row justify="center" align="center" class="text-center" no-gutters>
+      <v-col cols="12" md="4">
+        <v-btn
+          @click="goTo('time-tracking')"
+          variant="text"
+          size="200"
+          stacked
+          color="primary">
+          <v-icon size="150">mdi-clock-in</v-icon>
+          <span class="mt-2 text-lg font-semibold text-gray-800"
+            >Time track</span
+          >
+        </v-btn>
+      </v-col>
 
-    <v-container class="mx-auto">
-        <v-row justify="center" align="center" class="text-center" no-gutters>
-            <div class="text-h3 mb-10">Here goes option 1</div>
-            <!-- <v-col cols="12" md="4">
-                <v-btn @click="goTo('my-scenarios')" variant="text" size="200" stacked color="primary">
-                    <v-icon size="150">mdi-hospital-building</v-icon>
-                    <span class="mt-2 text-lg font-semibold text-gray-800">Mis escenarios</span>
-                </v-btn>
-            </v-col> -->
-
-            <div class="text-h3 mb-10">Here goes option 2</div>
-            <!-- <v-col cols="12" md="4">
-                <v-btn @click="goTo('simulations')" variant="text" size="200" stacked color="primary">
-                    <v-icon size="150">mdi-cog</v-icon>
-                    <span class="mt-2 text-lg font-semibold text-gray-800">Simulaciones</span>
-                </v-btn>
-            </v-col> -->
-
-            <div class="text-h3 mb-10">Here goes option 3</div>
-            <!-- <v-col cols="12" md="4">
-                <v-btn @click="goTo('optimizations')" variant="text" size="200" stacked color="primary">
-                    <v-icon size="150">mdi-clipboard-check</v-icon>
-                    <span class="mt-2 text-lg font-semibold text-gray-800">Optimizaciones</span>
-                </v-btn>
-            </v-col> -->
-        </v-row>
-    </v-container>
+      <v-col cols="12" md="4">
+        <v-btn
+          @click="goTo('time-history')"
+          variant="text"
+          size="200"
+          stacked
+          color="primary">
+          <v-icon size="150">mdi-history</v-icon>
+          <span class="mt-2 text-lg font-semibold text-gray-800"
+            >Tracker history</span
+          >
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
-const router = useRouter()
+  const router = useRouter();
 
-function goTo(path) {
-    router.push(`/${path}`)
-}
+  function goTo(path) {
+    router.push(`/${path}`);
+  }
 </script>
