70890965675ed606f1653e8ff154738993ce18df
David <dcs1460@gmail.com>
2025-07-06 02:55:21 +0200

    feat: Definition of the schemas related with the `TimeEntry`, `SubTask` and `Task` models of the database

---
+++ ticktask/server/schemas/tasks_schema.py
@@ -0,0 +1,36 @@
+"""
+Definiton of the schemas used in the tasks and subtasks definition
+"""
+
+from datetime import datetime
+
+from ninja import Schema
+
+
+class TimeEntrySchema(Schema):
+    """
+    Definition of the schema of each one of the
+    time entries of each subtask.
+    """
+
+    clock_in: datetime
+    clock_out: datetime
+
+
+class SubTaskSchema(Schema):
+    """
+    Definition of the schema of each individual subtask.
+    """
+
+    name: str
+    description: str
+    time_entries: list[TimeEntrySchema]
+
+
+class TaskSchema(Schema):
+    """
+    Definition of the schema of each individual task.
+    """
+
+    name: str
+    subtasks: list[SubTaskSchema]
