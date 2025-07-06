81156f273b374125197e89bdba4cb9a97627296d
David <dcs1460@gmail.com>
2025-07-06 02:54:08 +0200

    feat: Include methods in the models to check the time dedicated to each task or subtask

--- ticktask/models.py
+++ ticktask/models.py
@@ -2,6 +2,8 @@
 Definition of the database tables that will be used in our application.
 """
 
+from datetime import timedelta
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -31,6 +33,16 @@ class Task(models.Model):
     name = models.CharField(max_length=200)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")
 
+    def get_time_dedicated(self) -> timedelta:
+        """
+        This function will return the amount of time
+        dedicated to this individual task.
+        """
+        total_time = timedelta()
+        for time_entry in self.subtasks.all():  # pylint: disable=no-member
+            total_time += time_entry.get_time_dedicated()
+        return total_time
+
 
 class SubTask(models.Model):
     """
@@ -41,6 +53,16 @@ class SubTask(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=600)
     task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="subtasks")
+
+    def get_time_dedicated(self) -> timedelta:
+        """
+        This function will return the amount of time
+        dedicated to this individual subtask.
+        """
+        total_time = timedelta()
+        for time_entry in self.time_entries.all():  # pylint: disable=no-member
+            total_time += time_entry.get_time_dedicated()
+        return total_time
 
 
 class TimeEntry(models.Model):
@@ -54,3 +76,12 @@ class TimeEntry(models.Model):
     )
     clock_in = models.DateTimeField(auto_now_add=True)
     clock_out = models.DateTimeField(null=True, blank=True)
+
+    def get_time_dedicated(self) -> timedelta:
+        """
+        This function will return the amount of time
+        dedicated to this individual time entry.
+        """
+        if self.clock_out:
+            return self.clock_out - self.clock_in
+        return timedelta()
