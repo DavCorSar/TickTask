74a8d50e0c79ee1bf5f92e527d0e24e5255c7d02
David <dcs1460@gmail.com>
2025-07-07 20:06:24 +0200

    feat: Include a new route to the api

--- ticktask/api.py
+++ ticktask/api.py
@@ -5,6 +5,9 @@ Definition of the api object
 from ninja import NinjaAPI
 from ticktask.server.routes.auth import auth_router
 
+from ticktask.server.routes.tasks import ticktask_router
+
 api = NinjaAPI()
 
 api.add_router("/auth/", auth_router)
+api.add_router("/ticktask/", ticktask_router)
